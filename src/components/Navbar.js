e25b0b8ef6f057602bd4364dc06aabce3f712441
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-07 23:07:36 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="#about">About Us</a>
-            </li>
-            <li>
-                <a href="#schedule">Schedule</a>
-            </li>
-            <li>
-                <a href="#tracks">Tracks</a>
-            </li>
-            <li>
-                <a href="#sponsors">Sponsers</a>
-            </li>
-            <li>
-                <a href="#team">Team</a>
-            </li>
-            <li>
-                <a href="#faq">Faq</a>
-            </li>
-            <li>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
-      </nav>
-    </>
-  )
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    return (
+        <>
+            <nav className={openMenu ? "active" : ""}>
+                <ul>
+                    <li>
+                        <a href="#home" onClick={() => setOpenMenu(false)} >Home</a>
+                    </li>
+                    <li>
+                        <a href="#about" onClick={() => setOpenMenu(false)} >About Us</a>
+                    </li>
+                    <li>
+                        <a href="#schedule" onClick={() => setOpenMenu(false)} >Schedule</a>
+                    </li>
+                    <li>
+                        <a href="#tracks" onClick={() => setOpenMenu(false)} >Tracks</a>
+                    </li>
+                    <li>
+                        <a href="#sponsors" onClick={() => setOpenMenu(false)}>Sponsers</a>
+                    </li>
+                    <li>
+                        <a href="#team" onClick={() => setOpenMenu(false)} >Team</a>
+                    </li>
+                    <li>
+                        <a href="#faq" onClick={() => setOpenMenu(false)} >Faq</a>
+                    </li>
+                    <li>
+                        <a href="#contact" onClick={() => setOpenMenu(false)} >Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="menu-icon">
+                <FaBarsStaggered className={openMenu ? 'nav-icon open-menu active' : 'nav-icon open-menu'} onClick={() => setOpenMenu(true)} />
+                <FaXmark className={openMenu ? 'nav-icon close-menu active' : 'nav-icon close-menu'} onClick={() => setOpenMenu(false)} />
+            </div>
+        </>
+    )
 }
 
 export default Navbar
