5bb21839bb840de4c90a1b679e1040a5a7eb2bf2
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-10 00:17:17 +0530

    Added Sponsors content

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,15 +1,55 @@
 import React from 'react'
 
-const Sponsors = () => {
+const spnsrs = () => {
   return (
     <>
       <section className="section" id="sponsors">
         <div className="sponsors-section">
-        <h1 className='common-title'>Sponsors</h1>
+          <h1 className='common-title'>Sponsors</h1>
+          <div className="sponsor-boxes">
+            <div className="box">
+              <h2 className='spnsr-title'>Platinum Sponsors</h2>
+              <div className='spnsr-grid'>
+                <div className="spnsr-img">
+                  <img src="../Devfolio.png" alt="" />
+                </div>
+                <div className="spnsr-img">
+                  <img src="../Polygon.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <h2 className='spnsr-title'>Gold Sponsors</h2>
+              <div className='spnsr-grid'>
+                <div className="spnsr-img">
+                  <img src="../Filecoin.png" alt="" />
+                </div>
+                <div className="spnsr-img">
+                  <img src="../Replit.png" alt="" />
+                </div>
+                <div className="spnsr-img">
+                  <img src="../Solana.png" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <h2 className='spnsr-title'>Silver Sponsors</h2>
+              <div className='spnsr-grid'>
+                <div className="spnsr-img">
+                  <img src="../Verbwire.png" alt="" />
+                </div>
+              </div>
+            </div>
+            {/* <div className="box">
+              <h2 className='spnsr-title'>In-Kind Sponsors</h2>
+              <div className="spnsr-img"></div>
+            </div> */}
+          </div>
         </div>
       </section>
     </>
   )
 }
 
-export default Sponsors
+export default spnsrs
+
