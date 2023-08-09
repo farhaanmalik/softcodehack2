7ca74e8f080b296369287a6a615b72b260f465eb
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-09 19:48:36 +0530

    Add files via upload

---
+++ src/src/components/About.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <section className='section' id='about'>
+                <div className="about-section">
+                    <h2 className="common-title">About Us</h2>
+                    <div className="content">
+                        <p>
+                            Soft CodeHack 2.0 is an exciting and inspiring event, brought to you
+                            by the PHICSIT community. Our goal is to bring together individuals
+                            with a passion for technology, engineering, and sustainability, to
+                            collaborate and create innovative solutions that have the potential to
+                            make a real difference in the world.
+                        </p>
+                        <br />
+                        <p>
+                            We believe that collaboration and inclusivity are the keys to
+                            unlocking creativity and innovation, which is why we're committed to
+                            providing a welcoming and supportive environment for all participants.
+                            Whether you're an experienced developer or just starting out, whether
+                            you come from a technical background or not, you'll find a place at
+                            Soft CodeHack 2.0.
+                        </p>
+                        <br />
+                        <p>
+                            During the event, you'll have access to cutting-edge tools, resources,
+                            and mentors who will help you develop your ideas and bring them to
+                            life. You'll work alongside other like-minded individuals, and have
+                            the opportunity to learn new skills, build your network, and make a
+                            positive impact on the world.
+                        </p>
+                        <br />
+                        <p>
+                            If you're passionate about technology, engineering, and
+                            sustainability, and you want to be part of a collaborative and
+                            inclusive community working towards a better world, then Soft CodeHack
+                            2.0 is the event for you. Join us and let's hack our way to a brighter
+                            future together!
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default About
---
+++ src/src/components/Sponsors.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Sponsors = () => {
+  return (
+    <>
+      <section className="section" id="sponsors">
+        <div className="sponsors-section">
+        <h1 className='common-title'>Sponsors</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Sponsors
