f70b34ac7c75e782d11188f34d7ca83269983823
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-10 21:56:34 +0530

    Added Footer in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import './Timeline.css';
 import Home from './pages/Home';
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
 
     </>
