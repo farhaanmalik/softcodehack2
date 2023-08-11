6678ad6e7a343e92d0633369e71e656a933cbd20
Vishal Raj <9031842908.vk@gmail.com>
2023-08-11 20:34:59 +0530

    seo and fragmentation improvements
    Signed-off-by: Vishal Raj <9031842908.vk@gmail.com>

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+
+const Mentor = ({ image, name }) => (
+  <div className="team-box">
+    <div className="team-img">
+      <img src={image} alt={`${name}'s profile`} />
+    </div>
+    <div className="team-content">
+      <h3>{name}</h3>
+    </div>
+  </div>
+);
 
 const Team = () => {
-  const teams = [
+  const mentors = [
     {
       id: 1,
       image: "../team.jpg",
@@ -17,30 +28,20 @@ const Team = () => {
       image: "../team.jpg",
       name: "Farhaan Malik",
     }
-  ]
-  return (
-    <>
-      <section id='mentors' className='section'>
-        <div className="team-section">
-          <h1 className="common-title">Mentors</h1>
-          <div className="team">
-            {teams.map((item) => {
-              return (
-                <div className="team-box" key={item.id}>
-                  <div className="team-img">
-                    <img src={item.image} alt="" />
-                  </div>
-                  <div className="team-content">
-                    <h3>{item.name}</h3>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </div>
-      </section>
-    </>
-  )
-}
+  ];
 
-export default Team
+  return (
+    <section id='mentors' className='section'>
+      <div className="team-section">
+        <h1 className="common-title">Mentors</h1>
+        <div className="team">
+          {mentors.map((mentor) => (
+            <Mentor key={mentor.id} image={mentor.image} name={mentor.name} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
