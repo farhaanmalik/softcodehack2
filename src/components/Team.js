0bcd7909b71f32d1c7bb923796d8850f395c4dd6
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-08-13 13:30:07 +0530

    Mentors

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,47 +1,54 @@
 import React from 'react';
 
-const Mentor = ({ image, name }) => (
-  <div className="team-box">
-    <div className="team-img">
-      <img src={image} alt={`${name}'s profile`} />
-    </div>
-    <div className="team-content">
-      <h3>{name}</h3>
-    </div>
-  </div>
-);
-
 const Team = () => {
-  const mentors = [
+   const mentors = [
     {
       id: 1,
       image: "../team.jpg",
-      name: "Farhaan Malik",
+      name: "Name 1",
+      role: "Designation",
+      companyName:"Company Name"
     },
     {
       id: 2,
       image: "../team.jpg",
-      name: "Farhaan Malik",
+      name: "Name 2",
+      role: "Designation",
+      companyName:"Company Name"
     },
     {
       id: 3,
       image: "../team.jpg",
-      name: "Farhaan Malik",
+      name: "Name 3",
+      role: "Designation",
+      companyName:"Company Name"
     }
-  ];
-
+  ]
   return (
-    <section id='mentors' className='section'>
-      <div className="team-section">
-        <h1 className="common-title">Mentors</h1>
-        <div className="team">
-          {mentors.map((mentor) => (
-            <Mentor key={mentor.id} image={mentor.image} name={mentor.name} />
-          ))}
+    <>
+      <section id='mentors' className='section'>
+        <div className="team-section">
+          <h1 className="common-title">Mentors</h1>
+          <div className="team">
+            {mentors.map((item) => {
+              return (
+                <div className="team-box" key={item.id}>
+                  <div className="team-img">
+                    <img src={item.image} alt={`${item.name}-Profile`} />
+                  </div>
+                  <div className="team-content">
+                    <h3>{item.name}</h3>
+                    <p>{item.role}</p>
+                    <p>{item.companyName}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    </>
+  )
+}
 
 export default Team;
